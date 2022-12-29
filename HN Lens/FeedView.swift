b9b6ca13b0386cd6b0f9bccfdb6b0d2e39fecdb3
Victor Bitca <victorbitca13@gmail.com>
2022-12-29 21:02:02 +0200

    Remove content view

--- HN Lens/FeedView.swift
+++ HN Lens/FeedView.swift
@@ -1,28 +1,6 @@
 import SwiftUI
 import HackerNewsKit
 
-struct ContentView: View {
-   @State var frame: CGSize = .zero
-    
-    var body: some View {
-        HStack {
-            GeometryReader { (geometry) in
-                self.makeView(geometry)
-            }
-        }
-    }
-    
-    func makeView(_ geometry: GeometryProxy) -> some View {
-        print(geometry.size.width, geometry.size.height)
-
-        DispatchQueue.main.async { self.frame = geometry.size }
-
-        return Text("Test")
-                .frame(width: geometry.size.width)
-                .overlay(Color(.red))
-    }
-}
-
 struct FeedView: View {
     @ObservedObject var model: FeedModel
     @EnvironmentObject var coordinator: Coordinator
